4d7836c04f41a4e1ac9c7c879ff4b967e16e4c7e
Luis <guga.burigo29@outlook.com>
2019-12-13 16:19:46 -0300

    create class Screen

--- src/Screen.js
+++ src/Screen.js
@@ -1,9 +1,27 @@
 class Screen {
-
-    constructor(state) {
+    constructor({context, state}) {
         this.state = state;
+        this.context = context;
     }
 
+    renderScreen() {
+        this.context.fillStyle = 'white';
+        this.context.fillRect(0, 0, 10, 10);
+
+        for (let playerId in this.state.players) {
+            const player = this.state.players[playerId];
+            this.context.fillStyle = 'black';
+            this.context.fillRect(player.x, player.y, 1, 1)
+        }
+
+        for (let fuitId in this.state.fruits) {
+            const fruit = this.state.fruits[fuitId];
+            this.context.fillStyle = 'green';
+            this.context.fillRect(fruit.x, fruit.y, 1, 1)
+        }
+
+        requestAnimationFrame(this.renderScreen.bind(this))
+    }
 }
 
 export {Screen}
